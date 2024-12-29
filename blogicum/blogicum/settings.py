4851110cce0b46395b467b8d82c340bb35989762
Taisia Zinchenko <taisia.zinchenko@bk.ru>
2024-12-29 21:27:28 +0300

    version 1

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blogicum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_dev',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def about(request):
+    template = 'pages/about.html'
+    return render(request, template)
+
+
+def rules(request):
+    template = 'pages/rules.html'
+    return render(request, template)
